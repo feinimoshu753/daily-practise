320138800b85d26b9da1cb82acdd2c9ed68d1d13
hmj <hello_world@163.com>
2018-02-24 13:40:13 +0800

    优化

--- frontend/promise/promise.js
+++ frontend/promise/promise.js
@@ -7,11 +7,13 @@ function MyPromise(callback) {
     this.successDeps = [];
     this.errorDeps = [];
     this.value = '';
+    this.end = false;
     var resolve = function (value) {
         this.status = 'fulfilled';
         this.value = value;
         if (this.successDeps.length > 0) {
             this.successDeps[0](value);
+            this.end = true;
         }
     };
     var reject = function (value) {
@@ -19,28 +21,40 @@ function MyPromise(callback) {
         this.value = value;
         if (this.errorDeps.length > 0) {
             this.errorDeps[0](value);
+            this.end = true;
         }
     };
     callback(resolve.bind(this), reject.bind(this));
 }
 
 MyPromise.prototype.then = function (success, error) {
+    if(this.end === true){
+        return this;
+    }
+
     if (this.status === 'pending') {
         typeof success === 'function' && this.successDeps.push(success);
         typeof error === 'function' && this.errorDeps.push(error);
     } else if (this.status === 'fulfilled') {
         typeof success === 'function' && success(this.value);
+        this.end = true;
     } else if (this.status === 'rejected') {
         typeof error === 'function' && error(this.value);
+        this.end = true;
     }
     return this;
 };
 
 MyPromise.prototype.catch = function (error) {
+    if(this.end === true){
+        return this;
+    }
+
     if (this.status === 'pending') {
         typeof error === 'function' && this.errorDeps.push(error);
     } else if (this.status === 'rejected') {
         typeof error === 'function' && error(this.value);
+        this.end = true;
     }
     return this;
 }
